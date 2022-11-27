25b86b6fdd5530361e22ba077029f13195a40cea
SaicereT <justintrejos50@yahoo.com>
2022-11-27 17:59:28 +0000

    Second Commit

--- src/js/component/todo.jsx
+++ src/js/component/todo.jsx
@@ -1,4 +1,3 @@
-import { func } from "prop-types";
 import React, {useState, useEffect} from "react";
 
 const TodoList = () => {
@@ -7,45 +6,60 @@ const TodoList = () => {
 
     const [newTask, setNewTask]=useState("")
 
-    function addTask(e){
+    const apiURL= "https://assets.breatheco.de/apis/fake/todos/user/JustinTrejos"
+
+   async function addTask(e){
         if(e.code=="Enter" && newTask!=""){
+            if (tasks.length==0){
+                let resp=await fetch (apiURL,{
+                    method:"POST",
+                    body:JSON.stringify([]),
+                    headers:{
+                        "Content-Type":"application/json"
+                    }
+                })
+                if (resp.ok){
+                    console.log("List Created")
+                } else{
+                    console.error(resp.status + ": " + resp.statusText)
+                    return
+                }
+            }
             setTasks([...tasks, {label: newTask, done: false}])
             setNewTask("")
         }
     }
 
-    function removeTask (index){
-        let newArray = [...tasks]
-        newArray.splice(index, 1)
-        setTasks(newArray)
+   async function removeTask (index){
+        let newArr = [...tasks]
+        newArr.splice(index, 1)
+         if (newArr.length == 0){
+            let resp = await fetch(apiURL, {method:"DELETE"})
+            if (resp.ok){
+                console.log("List Deleted")
+            }
+         }
+        setTasks(newArr)
+    } 
+
+    function checkMark (index){
+        let newArr = [...tasks]
+        newArr[index]={...newArr[index], done:!newArr[index].done}
+        setTasks(newArr)
     }
 
-    function removeAllTasks (){
-        let newArray = []
-        setTasks(newArray)
-    }
-    
+    //Component did mount
     useEffect (async () => {
-        var response = await fetch("https://assets.breatheco.de/apis/fake/todos/user/JustinTrejos")
-        if (response.status == 404){
-            //Creating the list (Post)
-            response = await fetch("https://assets.breatheco.de/apis/fake/todos/user/JustinTrejos",{
-                method:"POST",
-                body: JSON.stringify([]),
-                headers:{
-                    "Content-Type":"application/json"
-                }
-            })
-            response = await fetch("https://assets.breatheco.de/apis/fake/todos/user/JustinTrejos")
-        } else if (!response.ok){
-            //Error Message
-            console.error("Unknown Error " + response.statusText)
-        }
-        //Load the data from the Body (Get)
-        var data =await response.json()
+        let resp = await fetch(apiURL)
+        if (resp.ok){
+        let data = await resp.json()
         setTasks(data)
+    } else {
+        console.error(resp.status + ": " + resp.statusText)
+    }
     }, [])
 
+    //Component did update (tasks)
     useEffect (async () =>{
         if (tasks.length>0){
             let resp = await fetch("https://assets.breatheco.de/apis/fake/todos/user/JustinTrejos",{
@@ -58,31 +72,20 @@ const TodoList = () => {
              if(resp.ok){
                 console.info("List Updated")
              }
-        } /* else {
-            let deletresp = await fetch("https://assets.breatheco.de/apis/fake/todos/user/JustinTrejos",{
-                method:"DELETE",
-                headers:{
-                    "Content-Type":"application/json"
-                }
-             })
-        } */
+        }
     }, [tasks])
 
-    const deleteList = async () => {
+
+    
+    async function deleteList () {
         let deletresp = await fetch("https://assets.breatheco.de/apis/fake/todos/user/JustinTrejos",{
-            method:"DELETE",
-            headers:{
-                "Content-Type":"application/json"
-            }
+            method:"DELETE"
          })
-         let response = await fetch("https://assets.breatheco.de/apis/fake/todos/user/JustinTrejos",{
-            method:"POST",
-            body: JSON.stringify([]),
-            headers:{
-                "Content-Type":"application/json"
-            }
-        })
-         response = await fetch("https://assets.breatheco.de/apis/fake/todos/user/JustinTrejos")
+         if (deletresp.ok){
+            console.log("List Deleted")
+        }
+         let newArr = []
+         setTasks(newArr)
     }
 
     return (
@@ -92,13 +95,20 @@ const TodoList = () => {
                 <h1 className="d-flex justify-content-center">todo List</h1>
                 <div className="input-group input-group-lg">
                      <input type="text" className="form-control" placeholder="What needs to be done?" onKeyDown={e=>addTask(e)} onChange={e=>setNewTask(e.target.value)} value={newTask} aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg"></input>
-                     <button type="button" onClick={()=>deleteList() && removeAllTasks()} className="btn btn-outline-danger">Clear List</button>
+                     <button type="button" onClick={()=>deleteList()} className="btn btn-outline-danger">Clear List</button>
                 </div>
                 <ul className="list-group list-group-flush">
                     {tasks.map((task, index)=>(
-                            <li key={index} className="list-group-item d-flex hover-hide justify-content-between align-items-start">
-                            <div><h5>{task.label}</h5></div>
-                            <button type="button" onClick={()=>removeTask(index)} className="btn btn-light"><i className="bi bi-check-lg"></i></button>
+                            <li key={index} className="list-group-item hover-hide">
+                            <div id="check" className="form-check d-flex justify-content-between">
+                                <div>
+                                <input onClick={()=>checkMark(index)} defaultChecked={task.done} className="form-check-input mt-2 me-2" type="checkbox" value={task.done} id="flexCheckDefault"></input>
+                                <h5>{task.label + ": " + (task.done ? "All Done!" : "Not yet!")}</h5>
+                                </div>
+                                <div>
+                                    <button type="button" onClick={()=>removeTask(index)} className="btn btn-outline-danger"><i className="bi bi-x-lg"></i></button>
+                                </div>                            
+                            </div>
                             </li>
                     ))}
                     <li className="list-group-item d-flex justify-content-between align-items-start">{tasks.length} items left.</li>
